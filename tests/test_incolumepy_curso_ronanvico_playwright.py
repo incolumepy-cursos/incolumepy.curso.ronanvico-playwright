f0ec485ab7659b28db804b7bd58fa88a3d613196
Britodfbr <britodfbr@gmail.com>
2022-03-19 07:37:43 -0300

    chore: unified control version

--- tests/test_incolumepy_curso_ronanvico_playwright.py
+++ tests/test_incolumepy_curso_ronanvico_playwright.py
@@ -1,5 +1,39 @@
-from incolumepy_curso_ronanvico_playwright import __version__
+from incolumepy_curso_ronanvico_playwright import configfile, versionfile, __version__
+import pytest
+import re
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+@pytest.mark.parametrize(
+    'file',
+    [
+        configfile,
+        versionfile,
+    ],
+)
+def test_exists_file(file):
+    assert file.is_file(), f"Ops .. {file=}"
+
+
+@pytest.mark.parametrize(
+    ['version', 'expected'],
+    [
+        (__version__, True),
+        ('1111.0.0', True),
+        ('0.111111.0', True),
+        ('0.0.111111111', True),
+        ('0.0.111-a.9999', True),
+        ('0.0.111.a.9999', False),
+        ('0.1.0-dev.0', True),
+        ('0.111111.0-alpha.11', True),
+        ('0.1.1111110-beta.111', True),
+        ('10.1.0-rc.111', True),
+        ('10.1.0-a-111', False),
+        ('0.1.0-a1', False),
+        ('0.1', False),
+        ('1', False),
+        ('0.1.0_a1', False),
+    ],
+)
+def test_version(version, expected):
+    regex = r'\d+(\.\d+){2}(-\w+\.\d+)?'
+    assert bool(re.compile(regex).fullmatch(version)) == expected
