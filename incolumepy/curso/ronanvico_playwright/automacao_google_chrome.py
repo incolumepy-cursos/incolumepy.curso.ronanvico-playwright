8495e6c596e4c8ed54fb9d32846b6bca38a69ef4
Britodfbr <britodfbr@gmail.com>
2022-03-20 07:59:35 -0300

    feat: acesso async; tratativa jupyter

--- incolumepy/curso/ronanvico_playwright/automacao_google_chrome.py
+++ incolumepy/curso/ronanvico_playwright/automacao_google_chrome.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from playwright.sync_api import sync_playwright
-
+import toml
+from pathlib import Path
 __author__ = "@britodfbr"  # pragma: no cover
+conf = Path(__file__).parents[3] / 'configure.toml'
 
 
 def example_oficial():
@@ -39,8 +41,27 @@ def example02():
         page.wait_for_timeout(5000)
 
 
+def example03():
+    """
+
+    :return:
+    """
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto('https://instagram.com')
+        page.fill('//*[@id=\"loginForm\"]/div/div[1]/div/label/input', value=toml.load(conf)['instagram']['user'])
+        page.fill('//*[@id=\"loginForm\"]/div/div[2]/div/label/input', value=toml.load(conf)['instagram']['pw'])
+        page.click('//*[@id=\"loginForm\"]/div/div[3]/button')
+        page.wait_for_timeout(2000)
+        page.click('/html/body/div[5]/div/div/div/div[3]/button[2]')
+        print(page.title())
+        page.wait_for_timeout(10000)
+
+
 def run():
-    example02()
+    example03()
+    # example02()
     # example01()
     # example_oficial()
 
